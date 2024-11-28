446e882ee6cffe61db0f7fdc5e3a1e9e9d0a09e8
Issa laye KANE <issa.k@deally.fr>
2024-11-28 03:26:10 +0000

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.use(express.static('web')) //serve our files in public statically
-app.listen(4300 , () => {
+app.listen(4100 , () => {
     console.log('starting ....')
 })
