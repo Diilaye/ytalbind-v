edaa91a17e98a5a5146611ebe5a1938336bb1a3f
Issa laye KANE <issa.k@deally.fr>
2024-12-19 14:38:36 +0000

    add news version

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ app.get('/' , (req,res) => {
         res.send(html);
     }
 });
+app.get('/:test' , (req,res) => {
+    try {
+        const filePath = path.join(__dirname, 'web', 'index.html');
+        let html = fs.readFileSync(filePath, 'utf-8');
+        res.send(html);
+
+    } catch (error) {
+        const filePath = path.join(__dirname, 'web', 'index.html');
+        let html = fs.readFileSync(filePath, 'utf-8');
+        res.send(html);
+    }
+});
 app.listen(5780 , () => {
     console.log('starting ....')
 })
