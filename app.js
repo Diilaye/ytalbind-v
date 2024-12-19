833188640511c633a844e27c501f2c126f45c5ab
Issa laye KANE <issa.k@deally.fr>
2024-12-19 14:35:01 +0000

    add news version

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express()
 app.use(express.static(path.join(__dirname, "web")));
+app.get('/' , (req,res) => {
+    try {
+        const filePath = path.join(__dirname, 'web', 'index.html');
+        let html = fs.readFileSync(filePath, 'utf-8');
+        res.send(html);
+
+    } catch (error) {
+        const filePath = path.join(__dirname, 'web', 'index.html');
+        let html = fs.readFileSync(filePath, 'utf-8');
+        res.send(html);
+    }
+});
 app.listen(5780 , () => {
     console.log('starting ....')
 })
