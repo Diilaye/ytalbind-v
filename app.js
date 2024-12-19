56814f547aed14c307ec6983cd96f53ca3baabc4
Issa laye KANE <issa.k@deally.fr>
2024-12-19 14:42:00 +0000

    add news version

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express()
+const fs = require('fs');
 app.use(express.static(path.join(__dirname, "web")));
 app.get('/' , (req,res) => {
     try {
